daf2b2f50c07f95c51e09e859510cd587736bf45
Martin <m.schultz@lucentcode.com>
2025-09-23 10:30:41 +0200

    test without dist

--- dist/components/chat/footer/ChatFooter.d.ts
+++
@@ -1,8 +0,0 @@
-import { JSX } from 'react';
-interface ChatFooterProps {
-    showInfo?: string;
-    className?: string;
-}
-declare const ChatFooter: ({ showInfo, className }: ChatFooterProps) => JSX.Element;
-export default ChatFooter;
-//# sourceMappingURL=ChatFooter.d.ts.map
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { default as Chatbox } from './App';
-export default Chatbox;
-//# sourceMappingURL=index.d.ts.map
